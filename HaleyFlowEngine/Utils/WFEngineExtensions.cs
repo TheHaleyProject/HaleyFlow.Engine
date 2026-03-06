b802967243ba7540b7d7db6757f9b3f5d69bf9f0
Quetzal <rmsmech@outlook.com>
2026-03-06 13:57:16 +0400

    fixes

--- HaleyFlowEngine/Utils/WFEngineExtensions.cs
+++ HaleyFlowEngine/Utils/WFEngineExtensions.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace Haley.Utils {
     public static class WFEngineExtensions {
-        public static async Task<IWorkFlowEngine> Build(this WorkFlowEngineMaker input, IAdapterGateway agw) {
+        public static async Task<IWorkFlowEngine> Build(this WorkFlowEngineMaker input, IAdapterGateway agw)  {
             //replace the sql contents, as only we know that.
             var adapterKey = await input.Initialize(agw); //Base names are already coming from the concrete implementation of DBInstanceMaker
             var dal = new MariaWorkFlowDAL(agw, adapterKey);
