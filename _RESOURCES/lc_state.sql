fcb345ac22e51ef8cf7bc68273dae9efce558aaf
Quetzal <rmsmech@outlook.com>
2026-03-22 11:37:07 +0400

    sql comma at the end.

--- _RESOURCES/lc_state.sql
+++ _RESOURCES/lc_state.sql
@@ -16,7 +16,7 @@
 
 
 -- Dumping database structure for lcstate
-CREATE DATABASE IF NOT EXISTS `lcstate` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+CREATE DATABASE IF NOT EXISTS `lcstate`; /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
 USE `lcstate`;
 
 -- Dumping structure for table lcstate.ack
